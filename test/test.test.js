590e751bd70108bb8b8eb5436287c8d252119e0d
Cris Fandino <sicrnujnuj@gmail.com>
2021-02-22 20:05:04 +0800

    more tests

--- test/test.test.js
+++ test/test.test.js
@@ -1,19 +1,19 @@
-process.env.NODE_ENV = "test";
-process.env.DB_USER = "dbuser_test";
-process.env.DB_PASSWORD = "12345_test";
+process.env.NODE_ENV = 'test'
+process.env.DB_USER = 'dbuser_test'
+process.env.DB_PASSWORD = '12345_test'
 
-const config = require("../index")
-const testConfig = require("../config/test")
-const defaultConfig = require("../config/default")
+const config = require('../index')
+const testConfig = require('../config/test')
+const defaultConfig = require('../config/default')
+const mergeDeep = require('../mergeDeep')
 
-const mergeConfig = {...defaultConfig,...testConfig}
+const mergeConfig = mergeDeep(defaultConfig, testConfig)
 
-
-describe("Config Test Environment",()=>{
-	it("should match the test values",()=>{
-		expect(config.appName).toBe("test_test")
-		expect(config.dbUser).toBe("dbuser_test")
-		expect(config.dbPassword).toBe("12345_test")
-		expect(config).toEqual(mergeConfig)
-	})
+describe('Config Test Environment', () => {
+  it('should match the test values', () => {
+    expect(config.appName).toBe('test_test')
+    expect(config.dbUser).toBe('dbuser_test')
+    expect(config.dbPassword).toBe('12345_test')
+    expect(config).toEqual(mergeConfig)
+  })
 })
